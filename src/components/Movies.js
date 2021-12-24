3b2c99ab6a7cf5efaab06c455dbc8782b4ceb2cb
Erin <errose10@gmail.com>
2021-12-23 22:41:34 -0500

    commit tree 407fc6a4cc434a31befe8a35023067ae820a4edb

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,19 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies</h1>
+        {movies.map((movie, index) => (
+          <div key={index}>
+            <h3>Name: {movie.name}</h3>
+            <p> Times:{movie.time}</p>
+            <p>Genres:</p>
+            <ul>
+              {movies.genres.map((genre, index) => (
+                <li key={index}>{genre}</li>
+              ))}
+            </ul>
+            </div>
+        ))}
     </div>
   );
 };
